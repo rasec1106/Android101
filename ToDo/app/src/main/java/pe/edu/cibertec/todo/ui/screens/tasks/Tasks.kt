7d4900c06ab016aa6bfe5614f50142cdb3eb6dee
Cesar Herrera <elch.herrera@gmail.com>
2023-04-30 13:07:51 -0500

    Implement DELETE logic

--- ToDo/app/src/main/java/pe/edu/cibertec/todo/ui/screens/tasks/Tasks.kt
+++ ToDo/app/src/main/java/pe/edu/cibertec/todo/ui/screens/tasks/Tasks.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -98,8 +100,17 @@ fun Tasks(){
 //            }
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 items(names) {name->
-                    Row {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         Text(text = name)
+                        IconButton(onClick = {
+                            names.remove(name)
+                        }) {
+                            Icon(Icons.Filled.Delete, null)
+                        }
                     }
                 }
             }
