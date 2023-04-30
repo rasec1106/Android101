391949d72f1a0da36e12132dac4b29f8b148f3d1
Cesar Herrera <elch.herrera@gmail.com>
2023-04-30 11:02:58 -0500

    Modify structure of Tasks Composable

--- ToDo/app/src/main/java/pe/edu/cibertec/todo/ui/screens/tasks/Tasks.kt
+++ ToDo/app/src/main/java/pe/edu/cibertec/todo/ui/screens/tasks/Tasks.kt
@@ -1,9 +1,12 @@
 package pe.edu.cibertec.todo.ui.screens.tasks
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,26 +14,25 @@ import pe.edu.cibertec.todo.ui.theme.ToDoTheme
 
 @Composable
 fun Tasks(){
-
-    val names = listOf("Lili","Diego","Jorge")
-
     /**
      * Instead of using a normal Column, we use a LazyColumn
      * It can be scrollable and has a flexible height
      */
-    LazyColumn(modifier = Modifier.fillMaxSize()){
-        items(names){
-            Text(text = it)
+
+    val names = listOf("Lili","Diego","Jorge")
+
+    Column {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text(text = "New name")},
+            value = "",
+            onValueChange = {}
+        )
+        LazyColumn(modifier = Modifier.fillMaxSize()){
+            items(names){
+                Text(text = it)
+            }
         }
-//        item {
-//            Text(text = "Prueba")
-//        }
-//        items(50) {
-//            Text(text = "$it item")
-//        }
-//        item {
-//            Text(text = "Cesar")
-//        }
     }
 }
 
