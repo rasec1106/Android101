eb0d3ff99328590d73f4b54e28fe37255f60a61a
Cesar Herrera <elch.herrera@gmail.com>
2023-04-30 09:32:33 -0500

    Add padding

--- Session02/app/src/main/java/pe/edu/cibertec/session02/ui/login/Login.kt
+++ Session02/app/src/main/java/pe/edu/cibertec/session02/ui/login/Login.kt
@@ -1,9 +1,6 @@
 package pe.edu.cibertec.session02.ui.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
@@ -12,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pe.edu.cibertec.session02.ui.theme.Session02Theme
 
 @Composable
@@ -25,7 +23,7 @@ fun Login(){
         Text(text = "Login")
         // onValueChange needs a function, so we can pass a {} at a first moment
         TextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
             label = { Text(text = "Username")},
             value = "",
             onValueChange = {},
@@ -33,7 +31,7 @@ fun Login(){
             leadingIcon = {Icon(Icons.Default.Person, null)}
         )
         TextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
             label = { Text(text = "Password")},
             value = "",
             onValueChange = {},
