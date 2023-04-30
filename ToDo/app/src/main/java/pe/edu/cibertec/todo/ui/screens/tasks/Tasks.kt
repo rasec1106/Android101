26c025dd8b2eb66211daf31879be8c33523b22ea
Cesar Herrera <elch.herrera@gmail.com>
2023-04-30 10:07:08 -0500

    Add variable items to LazyColumn

--- ToDo/app/src/main/java/pe/edu/cibertec/todo/ui/screens/tasks/Tasks.kt
+++ ToDo/app/src/main/java/pe/edu/cibertec/todo/ui/screens/tasks/Tasks.kt
@@ -1,9 +1,10 @@
 package pe.edu.cibertec.todo.ui.screens.tasks
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pe.edu.cibertec.todo.ui.theme.ToDoTheme
 
@@ -13,9 +14,15 @@ fun Tasks(){
      * Instead of using a normal Column, we use a LazyColumn
      * It can be scrollable and has a flexible height
      */
-    LazyColumn{
+    LazyColumn(modifier = Modifier.fillMaxSize()){
         item {
             Text(text = "Prueba")
+        }
+        items(5) {
+            Text(text = "$it item")
+        }
+        item {
+            Text(text = "Cesar")
         }
     }
 }
