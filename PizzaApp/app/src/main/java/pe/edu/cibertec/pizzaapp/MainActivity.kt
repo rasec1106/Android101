51ecb0f264adc20d6699fa8da02fdaef37284aa7
Cesar Herrera <elch.herrera@gmail.com>
2023-04-30 14:17:54 -0500

    Create frontend structure

--- PizzaApp/app/src/main/java/pe/edu/cibertec/pizzaapp/MainActivity.kt
+++ PizzaApp/app/src/main/java/pe/edu/cibertec/pizzaapp/MainActivity.kt
@@ -3,11 +3,14 @@ package pe.edu.cibertec.pizzaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pe.edu.cibertec.pizzaapp.ui.theme.PizzaAppTheme
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Home()
                 }
             }
         }
@@ -30,14 +33,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Home(modifier: Modifier = Modifier ) {
+
+    val isCheckedPizzaAmericana = remember {
+        mutableStateOf(false)
+    }
+    val isCheckedPizzaHawaiana = remember {
+        mutableStateOf(true)
+    }
+    val isCheckedPizzaVegetariana = remember {
+        mutableStateOf(false)
+    }
+
+    Column(modifier = modifier) {
+        TextField(value = "Name", onValueChange = {})
+        TextField(value = "Quantity", onValueChange = {})
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = isCheckedPizzaAmericana.value, onCheckedChange = {})
+            Text(text = "Pizza americana")
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(checked = isCheckedPizzaHawaiana.value, onCheckedChange = {})
+            Text(text = "Pizza hawaiana")
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(checked = isCheckedPizzaVegetariana.value, onCheckedChange = {})
+            Text(text = "Pizza vegetariana")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PizzaAppTheme {
-        Greeting("Android")
+        Home()
     }
 }
