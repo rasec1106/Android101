c74c1e792d7bf88958266d97fc40e6f10f86bf1b
Cesar Herrera <elch.herrera@gmail.com>
2023-05-07 09:58:14 -0500

    Create Contact class with annotations

---
+++ AgendaCompose/app/src/main/java/pe/edu/cibertec/agendacompose/data/model/Contact.kt
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.agendacompose.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contacts")
+data class Contact(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "firstname")
+    val name: String
+)
